6a0ed688dd6dff464e49524512b974bfd4e7f139
VshaunR <vramshaun@gmail.com>
2024-09-16 20:54:31 -0400

    using code sandbox template

--- script.js
+++ script.js
@@ -1,212 +1,123 @@
-/*
-
-{
-  "id": number,
-  "name": string,
-}
-
-An AssignmentGroup object, which looks like this:
-{
-  "id": number,
-  "name": string,
-  // the ID of the course the assignment group belongs to
-  "course_id": number,
-  // the percentage weight of the entire assignment group
-  "group_weight": number,
-  "assignments": [AssignmentInfo],
-}
-
-Each AssignmentInfo object within the assignments array looks like this:
-{
-  "id": number,
-  "name": string,
-  // the due date for the assignment
-  "due_at": Date string,
-  // the maximum points possible for the assignment
-  "points_possible": number,
-}
-
-An array of LearnerSubmission objects, which each look like this:
-{
-    "learner_id": number,
-    "assignment_id": number,
-    "submission": {
-      "submitted_at": Date string,
-      "score": number
-    }
-}
-
-Your goal is to analyze and transform this data such that the output of your program is an array of objects, each containing the following information in the following format:
-{
-    // the ID of the learner for which this data has been collected
-    "id": number,
-    // the learner’s total, weighted average, in which assignments
-    // with more points_possible should be counted for more
-    // e.g. a learner with 50/100 on one assignment and 190/200 on another
-    // would have a weighted average score of 240/300 = 80%.
-    "avg": number,
-    // each assignment should have a key with its ID,
-    // and the value associated with it should be the percentage that
-    // the learner scored on the assignment (submission.score / points_possible)
-    <assignment_id>: number,
-    // if an assignment is not yet due, it should not be included in either
-    // the average or the keyed dictionary of scores
-}
-
-*/
-let date = new Date();
-console.log(date.toLocaleDateString())
-let courseInfo = {
-  // should match course id in assignmentGroup
-  id:1,
-  name:'Java'
+// The provided course information.
+const ci = {
+  id: 451,
+  name: "Introduction to JavaScript"
 };
 
-let assignmentGroup = {
-  //just the id of the group
-   id:1000,
-  name:'Team 1',
-  //course id should match courseInfo ID
-  course_id:1,
-  group_weight:10,
-  assignments:[
-
-        {
-          id:1,
-          name:'Make arrays',
-          due_at: date ='9/17/2024',
-          points_possible:100,
-        },
-        {
-          id:2,
-          name:'List primative types in JS',
-          due_at: date = '9/7/2024',
-          points_possible:100,
-        },
-        {
-          id:3,
-          name:'Make a function',
-          due_at:date = '9/27/2024',
-          points_possible:100,
-        }
-
-
+// The provided assignment group.
+const ag = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
+    }
   ]
 };
 
-
-
-let learnerSubmission = [
-  
- {
-   // learner id is the individual id of a person
-   learner_id:1,
-   //assignment id is the id of the assignments array in the assignmentGroup object
-   assignment_id:1,
-   submission:{
-   submitted_at: '9/16/2024',
-   score:100,
+// The provided learner submission data.
+const submissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
   }
-    
- },
- {
-  // learner id is the individual id of a person
-  learner_id:1,
-  //assignment id is the id of the assignments array in the assignmentGroup object
-  assignment_id:2,
-  submission:{
-  submitted_at:'9/27/2024',
-  score:70,
- }
- 
-},
-{
-  // learner id is the individual id of a person
-  learner_id:1,
-  //assignment id is the id of the assignments array in the assignmentGroup object
-  assignment_id:3,
-  submission:{
-  submitted_at: '9/1/2024',
-  score:30,
- },
- 
-  
-},
-{
-  // learner id is the individual id of a person
-  learner_id:2,
-  //assignment id is the id of the assignments array in the assignmentGroup object
-  assignment_id:1,
-  submission:{
-  submitted_at: '9/11/2024',
-  score:30,
- },
- 
-
-},
-{
-  // learner id is the individual id of a person
-  learner_id:2,
-  //assignment id is the id of the assignments array in the assignmentGroup object
-  assignment_id:2,
-  submission:{
-  submitted_at: '9/19/2024',
-  score:30,
- },
- 
-  
-},{
-  // learner id is the individual id of a person
-  learner_id:3,
-  //assignment id is the id of the assignments array in the assignmentGroup object
-  assignment_id:2,
-  submission:{
-  submitted_at: '9/19/2024',
-  score:30,
- },
- 
-  
-}
-
-
 ];
 
-// console.log(learnerSubmission)
-
-function getLearnerData(courseInfo,assignmentGroup,learnerSubmission){
-  let arrOfObj = [];
-  let obj={};
-  //getting an array of unique ids
- let arr= getUniqueId(learnerSubmission);
-//  console.log(arr);
-      learnerSubmission.forEach((item,i)=>{
-
-
-
-      });
-}
-getLearnerData(courseInfo,assignmentGroup,learnerSubmission)
-
-
-
-
-
-
-
-//function to get array of unique ids
-function getUniqueId(arr){
-  let temp=0;
-  let idArr=[]
-  learnerSubmission.forEach((sub,i)=>{
-    // temp=sub.learner_id;
+function getLearnerData(ci, ag, submissions) {
+  // here, we would process this data to achieve the desired result.
+  let result= [];
+    let temp =0;
+    let obj={};
+    let score ;
+    submissions.forEach(el => {
+        // console.log(el)
+        if(temp !== el.learner_id){
+            obj={
+              id:el.learner_id,
+            };
+            temp = el.learner_id;
+            result.push(obj)
       
-    if(sub.learner_id !== temp){
-        idArr.push(sub.learner_id)
-        temp =sub.learner_id
-    }
-  });
-  // console.log(idArr)
-  return idArr;
+        }else{
+          // obj={
+          //   id:temp
+          // };
+          // result.push(obj)
+        }
+       
+    });
+    console.log(result)
+  // const result = [
+  //   {
+  //     id: 125,
+  //     avg: 0.985, // (47 + 150) / (50 + 150)
+  //     1: 0.94, // 47 / 50
+  //     2: 1.0 // 150 / 150
+  //   },
+  //   {
+  //     id: 132,
+  //     avg: 0.82, // (39 + 125) / (50 + 150)
+  //     1: 0.78, // 39 / 50
+  //     2: 0.833 // late: (140 - 15) / 150
+  //   }
+  // ];
+
+  return result;
 }
 
+const result = getLearnerData(ci, ag, submissions);
 
+// console.log(result);
